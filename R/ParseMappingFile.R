da2dab741dcc59a546f8946990712dbcafcc02cf
Eduardo <eduardo.contijoch@gmail.com>
2015-02-07 10:13:06 -0500

    Cleanup

---
+++ R/ParseMappingFile.R
@@ -0,0 +1,11 @@
+ParseMappingFile <- function(mapping_file) {
+  
+  #Import file
+  file <- read.csv(mapping_file)
+ # mapping <- na.omit(read.csv(file = standards_mapping_csv_file, sep = ",", fill = TRUE, header = TRUE, na.strings = "", colClasses = c(rep("character", 8), "numeric")))
+  
+  # Cast well names as characters
+  file$Well <- as.character(file$Well)
+  
+  return(file)
+}
