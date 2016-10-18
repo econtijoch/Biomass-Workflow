58df9120c960a829e33e08173cb0f6c77bb4a024
Eduardo <eduardo.contijoch@gmail.com>
2016-10-18 18:04:22 -0400

    bug fix

--- R/robot_prep.R
+++ R/robot_prep.R
@@ -88,7 +88,7 @@ robot_prep_16S <- function(dataset) {
     robot_table$WaterWell <- 1
     robot_table$NormalizedVolume <- robot_table$DNA_Vol + robot_table$Water_Vol
     
-    output <- as.data.frame(select(robot_table, WaterSource, WaterWell, DNASource, DNASourceWell, Destination, DestinationWell, DNA_Vol, Water_Vol, Warning, BarcodeID, PlateID, SampleWell, NormalizedVolume, StartingConc, FinalConc))
+    output <- as.data.frame(select(robot_table, WaterSource, WaterWell, DNASource, DNASourceWell, Destination, DestinationWell, DNA_Vol, Water_Vol, Warning, BarcodeID, PlateID, SampleWell, BarcodePlate, BarcodeWell, NormalizedVolume, StartingConc, FinalConc))
     
     
   }
