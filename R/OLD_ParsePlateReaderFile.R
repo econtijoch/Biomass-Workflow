3d17dc5a9037b926cb5a38da1046b1fe8af9d206
Eduardo Contijoch <eduardocon@eduardos-air.hcsm.mssm.edu>
2017-01-06 12:01:38 -0500

    test

--- R/OLD_ParsePlateReaderFile.R
+++ R/OLD_ParsePlateReaderFile.R
@@ -23,11 +23,30 @@ Deprecated_ParsePlateReaderFile <- function(plate_reader_file) {
     # Parse table
     parsed_table <- file[start_row:end_row, start_column:ncol(file)]
     
+    
+    
     # Add column names
     colnames(parsed_table) <- make.names(lapply(file[title_row, start_column:ncol(file)], as.character))
     
+    rows <- c("A", "B", "C", "D", "E", "F", "G", "H")
+    cols <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+    
+    rows_length <- length(rows)
+    cols_length <- length(cols)
+    
+    wells <- "Well"
+    for (r in 1:rows_length) {
+      for (c in 1:cols_length) {
+        
+        index <- (r - 1) * 12 + c
+        index_name <- paste(rows[r], cols[c], sep = "")
+        wells <- c(wells, index_name)
+        
+      }
+    }
+    
     # Cast well names as characters
-    parsed_table$Well <- as.character(parsed_table$Well)
+    parsed_table$ReaderWell <- wells[-1]
     
     
     # Cast as numeric
