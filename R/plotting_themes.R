ff53c5343c6c8727d4761e07052ebf551ab56eed
Eduardo Contijoch <eduardocon@eduardos-air.hcsm.mssm.edu>
2017-03-16 16:34:53 -0400

    edited plotting theme for facets (removed gray paneling)

--- R/plotting_themes.R
+++ R/plotting_themes.R
@@ -7,15 +7,32 @@
 
 EJC_theme_tilted <- function(base_size = 12, base_family = "sans", bgcolor = "default") {
     bgcol <- NULL
-    ret <- ggplot2::theme(rect = ggplot2::element_rect(fill = bgcol, linetype = 0, colour = NA), text = ggplot2::element_text(size = base_size, 
-        family = base_family, color = "black"), title = ggplot2::element_text(size = 24, hjust = 0.5), axis.title.x = ggplot2::element_text(size = 18, 
-        hjust = 0.5), axis.line = ggplot2::element_line(color = "black"), axis.text.x = ggplot2::element_text(size = 18, 
-        angle = 45, hjust = 1, color = "black"), axis.text.y = ggplot2::element_text(size = 18, color = "black"), axis.title.y = ggplot2::element_text(size = 18, 
-        hjust = 0.5), panel.grid.major.y = ggplot2::element_blank(), panel.grid.minor.y = ggplot2::element_blank(), 
-        panel.grid.major.x = ggplot2::element_blank(), panel.grid.minor.x = ggplot2::element_blank(), panel.border = ggplot2::element_blank(), 
-        panel.background = ggplot2::element_rect(fill = NA), legend.position = "bottom", legend.background = ggplot2::element_rect(fill = "gray95", 
-            colour = "black"), legend.key = ggplot2::element_rect(fill = NA), legend.key.size = ggplot2::unit(0.5, "cm"), 
-        legend.title.align = 0.5, legend.title = ggplot2::element_text(size = 14, face = "bold"))
+    ret <- ggplot2::theme(
+		rect = ggplot2::element_rect(fill = bgcol, linetype = 0, colour = NA), 
+		text = ggplot2::element_text(size = base_size, family = base_family, color = "black"), 
+		
+		title = ggplot2::element_text(size = 24, hjust = 0.5), 
+		
+		axis.title.x = ggplot2::element_text(size = 18,hjust = 0.5), 
+		axis.line = ggplot2::element_line(color = "black"), 
+		axis.text.x = ggplot2::element_text(size = 18, angle = 45, hjust = 1, color = "black"), 
+		axis.text.y = ggplot2::element_text(size = 18, color = "black"), 
+		axis.title.y = ggplot2::element_text(size = 18,hjust = 0.5), 
+		
+		panel.grid.major.y = ggplot2::element_blank(), 
+		panel.grid.minor.y = ggplot2::element_blank(), 
+        panel.grid.major.x = ggplot2::element_blank(), 
+		panel.grid.minor.x = ggplot2::element_blank(), 
+		panel.border = ggplot2::element_blank(), 
+        panel.background = ggplot2::element_rect(fill = NA), 
+		
+		legend.position = "bottom", 
+		legend.background = ggplot2::element_rect(fill = "gray95", colour = "black"), 
+		legend.key = ggplot2::element_rect(fill = NA), 
+		legend.key.size = ggplot2::unit(0.5, "cm"), 
+        legend.title.align = 0.5, 
+		legend.title = ggplot2::element_text(size = 14, face = "bold"),
+		strip.background = element_rect(colour = NA, fill = NA))
     ret
 }
 
@@ -28,15 +45,32 @@ EJC_theme_tilted <- function(base_size = 12, base_family = "sans", bgcolor = "de
 
 EJC_theme <- function(base_size = 12, base_family = "sans", bgcolor = "default") {
     bgcol <- NULL
-    ret <- ggplot2::theme(rect = ggplot2::element_rect(fill = bgcol, linetype = 0, colour = NA), text = ggplot2::element_text(size = base_size, 
-        family = base_family, color = "black"), title = ggplot2::element_text(size = 24, hjust = 0.5), axis.title.x = ggplot2::element_text(size = 18, 
-        hjust = 0.5), axis.line = ggplot2::element_line(color = "black"), axis.text.x = ggplot2::element_text(size = 18, 
-        hjust = 0.5, color = "black"), axis.text.y = ggplot2::element_text(size = 18, color = "black"), axis.title.y = ggplot2::element_text(size = 18, 
-        hjust = 0.5), panel.grid.major.y = ggplot2::element_blank(), panel.grid.minor.y = ggplot2::element_blank(), 
-        panel.grid.major.x = ggplot2::element_blank(), panel.grid.minor.x = ggplot2::element_blank(), panel.border = ggplot2::element_blank(), 
-        panel.background = ggplot2::element_rect(fill = NA), legend.position = "bottom", legend.background = ggplot2::element_rect(fill = "gray95", 
-            colour = "black"), legend.key = ggplot2::element_rect(fill = NA), legend.key.size = ggplot2::unit(0.5, "cm"), 
-        legend.title.align = 0.5, legend.title = ggplot2::element_text(size = 14, face = "bold"))
+    ret <- ggplot2::theme(
+		rect = ggplot2::element_rect(fill = bgcol, linetype = 0, colour = NA), 
+		text = ggplot2::element_text(size = base_size, family = base_family, color = "black"), 
+		
+		title = ggplot2::element_text(size = 24, hjust = 0.5), 
+		
+		axis.title.x = ggplot2::element_text(size = 18, hjust = 0.5), 
+		axis.line = ggplot2::element_line(color = "black"), 
+		axis.text.x = ggplot2::element_text(size = 18, color = "black"), 
+		axis.text.y = ggplot2::element_text(size = 18, color = "black"), 
+		axis.title.y = ggplot2::element_text(size = 18,hjust = 0.5), 
+		
+		panel.grid.major.y = ggplot2::element_blank(), 
+		panel.grid.minor.y = ggplot2::element_blank(), 
+        panel.grid.major.x = ggplot2::element_blank(), 
+		panel.grid.minor.x = ggplot2::element_blank(), 
+		panel.border = ggplot2::element_blank(), 
+        panel.background = ggplot2::element_rect(fill = NA), 
+		
+		legend.position = "bottom", 
+		legend.background = ggplot2::element_rect(fill = "gray95", colour = "black"), 
+		legend.key = ggplot2::element_rect(fill = NA), 
+		legend.key.size = ggplot2::unit(0.5, "cm"), 
+        legend.title.align = 0.5, 
+		legend.title = ggplot2::element_text(size = 14, face = "bold"),
+		strip.background = element_rect(colour = NA, fill = NA))
     ret
 }
 
@@ -89,7 +123,8 @@ paper_theme <-
         legend.key.size = ggplot2::unit(0.25, "cm"),
         legend.title.align = 0.5,
         legend.margin = ggplot2::margin(1.5,1.5,1.5,1.5),
-        legend.title = ggplot2::element_text(size = 8, face = "bold")
+        legend.title = ggplot2::element_text(size = 8, face = "bold"),
+		strip.background = element_rect(colour = NA, fill = NA)
       )
     return(ret)
   }
@@ -144,7 +179,8 @@ paper_theme_tilted <-
         legend.key.size = ggplot2::unit(0.25, "cm"),
         legend.title.align = 0.5,
         legend.margin = ggplot2::margin(1.5,1.5,1.5,1.5),
-        legend.title = ggplot2::element_text(size = 8, face = "bold")
+        legend.title = ggplot2::element_text(size = 8, face = "bold"),
+		strip.background = element_rect(colour = NA, fill = NA)
       )
     return(ret)
   }
