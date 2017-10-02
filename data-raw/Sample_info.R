96102cecb1b8bde6ce6165f0f8d2e83858c3fc29
Eduardo Contijoch <eduardocon@Eduardos-MacBook-Air.local>
2017-10-02 18:15:23 -0400

    updates to vignette

--- data-raw/Sample_info.R
+++ data-raw/Sample_info.R
@@ -1,5 +1,5 @@
 Sample_info_filename <- system.file("extdata", "Sample_SampleInfo.csv", package = "BiomassWorkflow")
 
-Sample_info <- utils::read.csv(Sample_info_filename)
+Sample_info <- readr::read_csv(Sample_info_filename)
 
 devtools::use_data(Sample_info, overwrite = TRUE)
